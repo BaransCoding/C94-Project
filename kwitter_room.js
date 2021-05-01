7c62117554095fec2f17eb0378f6f3855dbe02cf
BaransCoding <79487593+BaransCoding@users.noreply.github.com>
2021-05-01 16:21:12 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCW-UIg7RPzwUurRFmF6CZNV3wbvxjdLtA",
+    authDomain: "c94-project.firebaseapp.com",
+    databaseURL: "https://c94-project-default-rtdb.firebaseio.com",
+    projectId: "c94-project",
+    storageBucket: "c94-project.appspot.com",
+    messagingSenderId: "672669460566",
+    appId: "1:672669460566:web:bd91ec1cf51819a2a973f7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
